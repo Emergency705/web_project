3e70fa18842b71199e8d54ce2e0f1809051f921b
jeongkyueun <kyueun@swu.ac.kr>
2025-07-06 05:52:44 +0900

    refactor: 마이페이지 코드 구조 개선

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchMyInfo } from "../apis/auth";
 import type { UserInfo } from "../apis/auth";
