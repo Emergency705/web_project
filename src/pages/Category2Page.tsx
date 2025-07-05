7badb1a657ecaa20da036f881319ca10c7a3e7c9
이상원 <sangwon020214@gmail.com>
2025-07-06 06:03:26 +0900

    deploy: 배포 문제 해결

--- src/pages/Category2Page.tsx
+++ src/pages/Category2Page.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useRef } from "react";
+import React, { useState, useMemo } from "react";
 import CategoryTopBar from "../components/CategoryTopBar";
 import AnnouncementCard from "../components/AnnouncementCard";
 import CategoryFilterBar from "../components/CategoryFilterBar";
