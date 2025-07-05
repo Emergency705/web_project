cff58e8be65f91303ea170659d19fa472cf60182
김덕환 <126309682+IISweetHeartII@users.noreply.github.com>
2025-07-06 01:41:52 +0900

    feat: Purchase 관련 페이지 추가 - 구매 목록 및 상세 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import BottomNavBar from "./components/BottomNavBar";
 
@@ -11,62 +17,95 @@ import ProfilePage from "./pages/signup/ProfilePage";
 import ExtraPage from "./pages/signup/ExtraPage";
 import CompletePage from "./pages/signup/CompletePage";
 
+// 구매 관련 페이지
+import PurchaseListPage from "./pages/purchase/PurchaseListPage";
+import PurchaseDetailPage from "./pages/purchase/PurchaseDetailPage";
+import RegisterCommentPage from "./pages/purchase/RegisterCommentPage";
+
 // Protected Pages
 import HomePage from "./pages/HomePage";
 import MyPage from "./pages/MyPage";
 import CounselPage from "./pages/CounselPage";
-import PurchasePage from "./pages/PurchasePage";
 import Category1Page from "./pages/Category1Page";
 import Category2Page from "./pages/Category2Page";
 import Category3Page from "./pages/Category3Page";
 import Category4Page from "./pages/Category4Page";
-import SavedPosts from './pages/SavedPosts';
+import EditProfile from "./pages/EditProfile";
+import GoodbyePage from "./pages/GoodbyePage";
+import MyFundings from "./pages/MyFundings";
+import WriteReview from "./pages/WriteReview";
+import SavedPosts from "./pages/SavedPosts";
 
+// 인증된 사용자를 위한 레이아웃 (조건부 BottomNavBar)
+const ProtectedLayout = () => {
+  const location = useLocation();
+  const hideNavBarPaths = [
+    /^\/purchase\/\d+$/, // 구매 상세 페이지
+    /^\/purchase\/\d+\/comment$/, // 댓글 작성 페이지
+  ];
 
-// 인증된 사용자를 위한 공통 레이아웃
-const MainLayout = () => (
-  <div style={{ paddingBottom: 60, minHeight: "100vh", background: "#fafafa" }}>
-    <Outlet /> 
-    <BottomNavBar />
+  const shouldHideNavBar = hideNavBarPaths.some((path) =>
+    path.test(location.pathname)
+  );
+
+  return (
+    <div
+      style={{
+        paddingBottom: shouldHideNavBar ? 0 : 60,
+        minHeight: "100vh",
+        background: "#ffffff",
+      }}
+    >
+      <Outlet />
+      {!shouldHideNavBar && <BottomNavBar />}
+    </div>
+  );
+};
+
+// 인증이 필요 없는 페이지들을 위한 공통 레이아웃
+const PublicLayout = () => (
+  <div style={{ minHeight: "100vh", background: "#fafafa" }}>
+    <Outlet />
   </div>
 );
 
-import EditProfile from "./pages/EditProfile"; // 추가
-import GoodbyePage from "./pages/GoodbyePage"; // 추가 (탈퇴 후 이동)
-import MyFundings from "./pages/MyFundings"; // 추가 (내가 참여한 펀딩 목록)
-import WriteReview from "./pages/WriteReview";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 인증이 필요 없는 페이지 (공통 레이아웃 없음) */}
-        <Route path="/" element={<IntroPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup/terms" element={<TermsPage />} />
-        <Route path="/signup/account" element={<AccountPage />} />
-        <Route path="/signup/profile" element={<ProfilePage />} />
-        <Route path="/signup/extra" element={<ExtraPage />} />
-        <Route path="/signup/complete" element={<CompletePage />} />
+        {/* 인증이 필요 없는 페이지 */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<IntroPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup/terms" element={<TermsPage />} />
+          <Route path="/signup/account" element={<AccountPage />} />
+          <Route path="/signup/profile" element={<ProfilePage />} />
+          <Route path="/signup/extra" element={<ExtraPage />} />
+          <Route path="/signup/complete" element={<CompletePage />} />
+        </Route>
 
-        {/* 인증이 필요한 페이지 (공통 레이아웃 + ProtectedRoute 적용) */}
+        {/* 인증이 필요한 페이지 */}
         <Route
           element={
             <ProtectedRoute>
-              <MainLayout />
+              <ProtectedLayout />
             </ProtectedRoute>
           }
         >
           <Route path="/home" element={<HomePage />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/counsel" element={<CounselPage />} />
-          <Route path="/purchase" element={<PurchasePage />} />
+          <Route path="/purchase" element={<PurchaseListPage />} />
+          <Route path="/purchase/:id" element={<PurchaseDetailPage />} />
+          <Route
+            path="/purchase/:id/comment"
+            element={<RegisterCommentPage />}
+          />
           <Route path="/edit-profile" element={<EditProfile />} />
           <Route path="/goodbye" element={<GoodbyePage />} />
           <Route path="/my-fundings" element={<MyFundings />} />
           <Route path="/write-review/:id" element={<WriteReview />} />
           <Route path="/saved-posts" element={<SavedPosts />} />
-
-          {/* 카테고리별 페이지 라우트 */}
           <Route path="/category1" element={<Category1Page />} />
           <Route path="/category2" element={<Category2Page />} />
           <Route path="/category3" element={<Category3Page />} />
